628fc00bc0c1270ada7307dc12401476c92d58b0
aaryakini <kinia313@newschool.edu>
2021-04-12 13:50:09 +0800

    Update sketch3.js

--- js/sketch3.js
+++ js/sketch3.js
@@ -4,91 +4,150 @@ function setup(){
 }
 
 function draw() {
-	background('grey');
+	background('#99B8FF');
 	noStroke();
-	//fill('#55a630');
-	//rect(100,0,600,800);
+	fill('#709BFF');
+	rect(200,0,400,800);
 	angleMode(DEGREES);
 
 	//right ear
-	fill('#FFFFFF');
-	ellipse(550,450,300,300);
+	fill('#f4845f');
+	ellipse(550,550,300,300);
 
-	//smaller circle
-	stroke('black');
+	//background colours
+	noStroke();
+	fill('#f4845f');
+	arc(550,550,240,240,0,45);
+	fill('#F79D65');
+	arc(550,550,240,240,45,90);
+	fill('#f4845f');
+	arc(550,550,240,240,90,135);
+	fill('#F79D65');
+	arc(550,550,240,240,135,180);
+	fill('#f4845f');
+	arc(550,550,240,240,180,225);
+	fill('#F79D65');
+	arc(550,550,240,240,225,270);
+	fill('#f4845f');
+	arc(550,550,240,240,270,315);
+	fill('#F79D65');
+	arc(550,550,240,240,315,0);
+
+	//inner circles
+	noFill();
+	stroke('#EF382E');
 	strokeWeight(3);
-	ellipse(550,450,240,240);
+	ellipse(550,550,255,255);
+	strokeWeight(5);
+	ellipse(550,550,240,240);
 
-	//pattern
-	stroke('red');
-	line(550,330,550,570);
-	line(430,450,670,450);
-	stroke('black');
+	//inner pattern
+	strokeWeight(3);
 	noFill();
 	//open up
-	arc(550,365,170,170,0,180);
+	arc(550,465,170,170,0,180);
 	//open down
-	arc(550,535,170,170,180,0);
+	arc(550,635,170,170,180,0);
 	//open right
-	arc(465,450,170,170,270,90);
+	arc(465,550,170,170,270,90);
 	//open left
-	arc(635,450,170,170,90,270);
+	arc(635,550,170,170,90,270);
 	//open NE
-	arc(610,390,170,170,45,225); //550 - 25 + 85 = 610
+	arc(610,490,170,170,45,225);
 	//open SE
-	arc(610,510,170,170,135,315);
+	arc(610,610,170,170,135,315);
 	//open NW
-	arc(490,390,170,170,315,135);
+	arc(490,490,170,170,315,135);
 	//open SW
-	arc(490,510,170,170,225,45);
+	arc(490,610,170,170,225,45);
 
 	//earring hole
 	noStroke();
-	fill('#264653');
-	ellipse(550,315,12,12);
+	fill('#709BFF');
+	ellipse(550,412,12,12);
 
 	//earring loop
-	fill('silver');
-	stroke('silver');
+	fill('gold');
+	stroke('gold');
 	strokeWeight(6);
-	line(550,315,550,265);
+	line(550,412,550,365);
 	noStroke();
-	ellipse(550,265,25,34);
-	rect(542.5,226,15,26);
-	stroke('silver');
+	ellipse(550,365,25,34);
+	rect(542.5,326,15,26);
+	stroke('gold');
 	strokeWeight(6);
 	noFill();
-	line(550,226,550,201);
-	arc(570,201,40,40,180,0);
-	line(590,201,590,246);
-	line(590,246,595,261);
-
-
+	line(550,326,550,301);
+	arc(570,301,40,40,180,0);
+	line(590,301,590,346);
+	line(590,346,595,361);
 
 	//left ear
 	noStroke();
-	fill('#FFFFFF');
+	fill('#f4845f');
 	ellipse(250,350,300,300); //x = -300 y = -100
 
-	//smaller circle
-	stroke('black');
+	//background colours
+	noStroke();
+	fill('#f4845f');
+	arc(250,350,240,240,0,45);
+	fill('#F79D65');
+	arc(250,350,240,240,45,90);
+	fill('#f4845f');
+	arc(250,350,240,240,90,135);
+	fill('#F79D65');
+	arc(250,350,240,240,135,180);
+	fill('#f4845f');
+	arc(250,350,240,240,180,225);
+	fill('#F79D65');
+	arc(250,350,240,240,225,270);
+	fill('#f4845f');
+	arc(250,350,240,240,270,315);
+	fill('#F79D65');
+	arc(250,350,240,240,315,0);
+
+	//inner circles
+	noFill();
+	stroke('#EF382E');
 	strokeWeight(3);
+	ellipse(250,350,255,255);
+	strokeWeight(5);
 	ellipse(250,350,240,240);
+
+	//inner pattern
+	strokeWeight(3);
+	noFill();
+	//open up
+	arc(250,265,170,170,0,180);
+	//open down
+	arc(250,435,170,170,180,0);
+	//open right
+	arc(165,350,170,170,270,90);
+	//open left
+	arc(335,350,170,170,90,270);
+	//open NE
+	arc(310,290,170,170,45,225);
+	//open SE
+	arc(310,410,170,170,135,315);
+	//open NW
+	arc(190,290,170,170,315,135);
+	//open SW
+	arc(190,410,170,170,225,45);
 
 	//earring hole
 	noStroke();
-	fill('#264653');
-	ellipse(250,215,12,12);
+	fill('#709BFF');
+	ellipse(250,212,12,12);
 
 	//earring loop
-	fill('silver');
-	stroke('silver');
+	fill('gold');
+	stroke('gold');
 	strokeWeight(6);
-	line(250,215,250,165);
+	line(250,212,250,165);
 	noStroke();
 	ellipse(250,165,25,34);
 	rect(242.5,126,15,26);
-	stroke('silver');
+	stroke('gold');
 	strokeWeight(6);
 	noFill();
 	line(250,126,250,101);
