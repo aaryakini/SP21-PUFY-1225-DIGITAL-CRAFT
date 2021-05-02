f0035d65ed51d687f505b798eb87e2785b4021df
aaryakini <kinia313@newschool.edu>
2021-05-02 11:24:36 +0800

    Updates

--- js/6sketch3.js
+++ js/6sketch3.js
@@ -2,21 +2,19 @@ let fr = 8;
 let a = 0;
 let m = 0;
 
-
 function setup() {
-	frameRate(fr);
-	let canvas = createCanvas(800,800,SVG);
-	canvas.style = "position:absolute; left: 50%; width: 400px; margin-left: -200px;";
-	colorMode(HSB, 360, 100, 100);
-	//noLoop();
-    noStroke();
-    alert("Flash Warning - Depending on the diameter entered in the next prompt, this sketch may contain colours that flash rapidly! To avoid excessive flashing, enter a smaller diameter.");
-	a = parseInt(window.prompt("Enter circle diameter - "));
-    m = width%a;
+  frameRate(fr);
+  let canvas = createCanvas(800,800,SVG);
+  colorMode(HSB, 360, 100, 100);
+  //noLoop();
+  noStroke();
+  alert("Flash Warning - Depending on the diameter entered in the next prompt, this sketch may contain colours that flash rapidly! To avoid excessive flashing, enter a smaller diameter.");
+  a = parseInt(window.prompt("Enter circle diameter - "));
+  m = width%a;
 }
 
 function draw() {
-  background('white');
+  background('black');
   //let counter1 = 0;
   for (let i = (m + (a/2)); i <= (width - a/2); i += a) {
   	//counter1++;
---
+++ js/6sketch4.js
@@ -0,0 +1,80 @@
+let fr = 2;
+let mainCounter = 0;
+let circles = [];
+let a = "";
+
+function setup() {
+	frameRate(fr);
+	let canvas = createCanvas(800,800);
+	canvas.center('horizontal');
+	colorMode(HSB, 360, 100, 100);
+	noStroke();
+	let d = 0;
+	while (d == 0){
+		a = window.prompt("Black or Gradient background?\nEnter B for black\nEnter G for gradient");
+		if (a == "B" || a =="b"){
+			d = 1;
+			background('black');
+		}
+		else if (a == "G" || a =="g"){
+			d = 1;
+			drawGradient();
+		}
+	}
+}
+
+function draw(){
+	if(mainCounter < 6){
+		let xPos = random(10,800);
+		let yPos = random(10,800);
+		let counter = 0
+		drawShape(xPos,yPos);
+		mainCounter++;
+		//console.log("mainCounter = " + mainCounter);
+	}
+	else{
+		clear();
+		if (a == "B" || a =="b")
+			background('black');
+		else if (a == "G" || a == "g")
+			drawGradient();
+		mainCounter = 0;
+	}  
+}
+
+function drawShape(x,y){
+	let counter = 0;
+	let s = random(1,10);
+	let scale = random(0,700);
+	let j = (height-scale)/1.1;
+	let diff = 40;
+	for(let i = (width-scale)/1.4; i > 10/1.2; i-=diff){
+		stroke('white');
+		strokeWeight(s);
+		noFill();
+		circles[counter] = ellipse(x,y,i,j);
+		j = j - diff;
+		counter++;
+	}
+}
+
+function drawGradient(){
+	let h = random(130, 360);
+	let r = 0;
+	for (let r = width*1.5; r > 0; r-=10){
+		noStroke();
+		fill(h, 50, 100);
+		ellipse(400, 400, r, r);
+		h = (h + 1) % 360;
+	}
+}
+
+function keyPressed(){
+	if (keyCode === ENTER){
+		noLoop();
+		saveCanvas(canvas,'myEllipsePattern','png');
+	}
+	else if (keyCode === 32){
+		loop();
+	}
+}
